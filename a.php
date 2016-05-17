784a2e0cbcc40a6eaaf36c22e71a1ed9648f1922
maievshadow <fz.uuuu@gmail.com>
2016-05-16 21:50:58 -0400

    doctrine--demo

---
+++ a.php
@@ -0,0 +1,12 @@
+<?php
+require_once "bootstrap.php";
+
+$newProductName = $argv[1];
+
+$product = new Product();
+$product->setName($newProductName);
+
+$entityManager->persist($product);
+$entityManager->flush();
+
+echo "Created Product with ID " . $product->getId() . "\n";
---
+++ b.php
@@ -0,0 +1,9 @@
+<?php
+require_once "bootstrap.php";
+
+$productRepository = $entityManager->getRepository('Product');
+$products = $productRepository->findAll();
+
+foreach ($products as $product) {
+    echo sprintf("-%s\n", $product->getName());
+}
