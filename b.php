895f534ec4ef69f9f20ec498def3a1d8b338cc76
maievshadow <fz.uuuu@gmail.com>
2017-10-30 11:42:55 +0800

    adjust

--- b.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "bootstrap.php";
-
-$productRepository = $entityManager->getRepository('Product');
-$products = $productRepository->findAll();
-
-foreach ($products as $product) {
-    echo sprintf("-%s\n", $product->getName());
-}
--- config/cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-// replace with file to your own project bootstrap
-require_once 'bootstrap.php';
-
-// replace with mechanism to retrieve EntityManager in your app
-
-return ConsoleRunner::createHelperSet($entityManager);
